c392da904a8edcbea8b744b40aaeada7d8b7d0d0
JaiR10vr <jair10.vasquez11@gmail.com>
2021-10-23 21:58:12 -0500

    Heroku

--- ejemploHandlebar/src/main/java/mx/uv/App.java
+++ ejemploHandlebar/src/main/java/mx/uv/App.java
@@ -13,6 +13,9 @@ public class App {
         System.out.println("Hello world");
         staticFiles.location("/");
 
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
+
 
         get("/handleB",(req,res)->{
             Map<String,String> model = new HashMap<>();
@@ -27,4 +30,13 @@ public class App {
     
 
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
